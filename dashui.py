27bd70f920d2f72be94e04dd5a3ccec1e19ca0ac
Prajwal Mani <prajwalmani@users.noreply.github.com>
2021-03-14 01:03:55 +0530

    dash ui commit

--- dashui.py
+++ dashui.py
@@ -17,49 +17,182 @@ project_name= 'Sentiment Analysis'
 
 def ui():
     df=pd.read_csv(r'estypredictedreviews.csv')
+    dfn=df[df['predictedvalue']==0] 
+    dfn=dfn.iloc[:6,:]
+    dfp=df[df['predictedvalue']==1]
+    dfp=dfp.iloc[:6,:]
+    df1=pd.concat([dfp,dfn])
+
     pie_chart=px.pie(
         data_frame=df,
         values=[df['predictedvalue'].value_counts()[1],df['predictedvalue'].value_counts()[0]],
         names=['Positive Reviews','Negative Reviews'],
         color=['Positive Reviews','Negative Reviews'],
         color_discrete_sequence=['Red','Orange'],
-        title='Title',
-        width=800,                          #figure width in pixels
-        height=600,                         #figure height in pixels
+        title='Positive and Negative Reviews Distribution',
+        width=600,                          
+        height=380,                         
         hole=0.5, 
     )
-    pio.show(pie_chart)
 
-    layout= html.Div(
-        # [
-        #     html.H1(id='title',children="Sentiment Analysis Using Insights"),
-        #     dcc.Textarea(
-        #         id='textarea',
-        #         placeholder="Enter the review text here",
-        #         style={'width':'100%','height':'100'}
-        #     ),
-        #     dbc.Button(
-        #         id='submit_button'
-        #         ,children='check review'
-        #         ,color='dark'
-        #         ,style={'width':'100%','height':'100'}),
-        #     html.H1(id='result',children=None)
-        # ]
-        
-        # [
-        #     dcc.Dropdown(id='dropdown',
-        #     options=[{ 'value': idx} for i in df2],
-        #     value=''
-        #     )
-        # ]
-        [
+    layout= html.Div([
+         dbc.Row([
+            dbc.Col(
+               html.Div([
             dcc.Graph(
                 id='pie_graph',
                 figure=pie_chart
             )
         ]
+        )
+            ),
+        dbc.Col(
+            html.Div(
+            [
+            html.H2(children='Etsy reviews'),
+            dcc.Dropdown(id='dropdown',
+           placeholder="Select exisiting Review",
+            options=[{'label':i,'value':i}for i in df1.review],
+            value='Select the etsy reviews',
+            optionHeight=70,
+            style = {'margin-bottom': '30px','min-width':'670px','padding-top':'25px'}
+            ),
+            dbc.Button(              
+               "Submit", 
+                id='submitdropdown',
+                color="dark", 
+                className="mr-1",
+                n_clicks=0,
+                style={'margin':'0 45%','padding':'5px 15px'}
+                ),
+            html.Div(id='container1',style={'padding-top':'15px'})
+            ]
+        )
+        )    
+         ]
+        ),
+        dbc.Row(
+            [
+            dbc.Col([
+            
+            html.Div(
+            [   
+                html.Div([
+                html.H2('Word Cloud'),
+                dbc.Button("ALL Words",
+                 id="allbt",
+                 outline=True,
+                 color="info", 
+                 className="mr-1",
+                 n_clicks_timestamp=0,
+                 style={'padding':'10px','padding-right':'15px'}
+                 ),
+                dbc.Button("Positve Words",
+                id="posbt",
+                 outline=True,
+                 color="success", 
+                 className="mr-1",
+                 n_clicks_timestamp=0,
+                 style={'padding':'10px','padding-right':'15px'}
+                 ),
+                dbc.Button("Negative Words",
+                id="negbt",
+                outline=True, 
+                color="danger",
+                className="mr-1",
+                n_clicks_timestamp=0,
+                style={'padding':'10px','padding-right':'15px'}
+                )
+                ],style={'padding-left':'15px'}
+                ),
+                html.Div(id='container',style={'padding':'15px'})
+            ]
+        ),
+            ]
+        ),
+        dbc.Col([
+            html.H2("Try it yourself!"),
+            html.Div(
+         [
+              dcc.Textarea(
+                id='textarea',
+                placeholder="Enter Your review text here",
+                rows=5,
+                # cols=8,
+                style={'width':'650px','height':'300'}
+            ),
+            html.Div(id='container2',style={'padding':'15px 15px 15px 10px'})
+         ]
+     )   
+        ]
+        )
+                
+            ]
+        )
+    ]
     )
     return layout
+
+@app.callback(
+    Output('container','children'),
+    [
+        Input('allbt','n_clicks_timestamp'),
+        Input('posbt','n_clicks_timestamp'),
+        Input('negbt','n_clicks_timestamp'),
+    ]
+)
+def wordcloudbutton(allbt,posbt,negbt):
+
+    if int(allbt) > int(posbt) and int(allbt)>int(negbt):
+        return html.Div([
+            html.Img(src=app.get_asset_url('wholeword.png'))])
+    elif int(posbt) > int(allbt) and int(posbt)>int(negbt):
+        return html.Div([
+            html.Img(src=app.get_asset_url('posword.png'))
+            ])
+    elif int(negbt) > int(allbt) and int(negbt) > int(posbt):
+       return html.Div([
+           html.Img(src=app.get_asset_url('wholeword.png'))
+           ])
+    else:
+        pass
+    
+@app.callback(
+    Output('container1','children'),
+    [
+        Input('submitdropdown','n_clicks')
+    ],
+    State('dropdown','value')
+)
+def dropdownui(n_clicks,value):
+    predict=preprocessingnpredictions(value)
+    if (n_clicks>0):
+        if(int(predict)==1):
+            return html.Div([
+                dbc.Alert("Its a Positive review", color="success")
+                ])
+        else:
+            return html.Div([
+                    dbc.Alert("Its a Negative review", color="danger")
+                ])
+
+@app.callback(
+    Output('container2','children'),
+    [
+        Input('textarea','value')
+    ]
+)
+def updatetextarea(textvalue):
+    predicted_value=preprocessingnpredictions(textvalue)
+    if(int(predicted_value)==1):
+            return html.Div([
+                dbc.Alert("Its a Positive review", color="success")
+                ])
+    else:
+        return html.Div([
+                dbc.Alert("Its a Negative review", color="danger")
+                ])
+
 
 def main():
     global app
---
+++ scrapcvs.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+from glob import glob
+
+file='Clothing_Shoes_and_Jewelry.json'
+
+df_json=pd.read_json(file,chunksize=1000000,lines=True)
+counter=1
+
+
+# for chunk in df_json:
+#     new_df=pd.DataFrame(chunk[['overall','reviewText','summary']])
+#     new_df1=new_df[new_df['overall']==5].sample(4000)
+#     new_df2=new_df[new_df['overall']==4].sample(4000)
+#     new_df3=new_df[new_df['overall']==3].sample(8000)
+#     new_df4=new_df[new_df['overall']==2].sample(4000)
+#     new_df5=new_df[new_df['overall']==1].sample(4000)
+#     new_df6=pd.concat([new_df1,new_df2,new_df3,new_df4,new_df5],axis=0,ignore_index=True)
+#     new_df6.to_csv(f"{counter}.csv",index=False)
+#     new_df=None
+#     print("We are in",counter)
+#     counter+=1
+
+file_names=glob("*.csv")
+dataframes=[pd.read_csv(f) for f in file_names]
+frame=pd.concat(dataframes,axis=0,ignore_index=True)
+frame.to_csv('balanced_review.csv',index=False)
+frame.info()
